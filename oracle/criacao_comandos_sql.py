7480e95c214cbad2375f71f4bf894d8cebc004ba
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-19 08:58:07 -0300

    Arrumando a importacao de municipio e estado

--- oracle/criacao_comandos_sql.py
+++ oracle/criacao_comandos_sql.py
@@ -1,4 +1,7 @@
-arq = open("municipio.txt")
+arq = open("municipio.txt", encoding='utf8')
+saida = open("comandos.sql", encoding="utf8", mode="w")
+
+dic_ufs = {}
 
 for registro in arq:
     #print(registro)
@@ -7,6 +10,18 @@ for registro in arq:
     uf = campos[0]
     cod_mun = int(campos[2])
     mun = campos[3]
+    mun = mun.replace('\n', '')
 
-    print(f"insert into estado(id, uf) values({cod_uf}, '{uf}');")
-    print(f"insert into municipio values({cod_mun}, '{mun}', {cod_uf});")
+    if not "'" in mun:        
+        #print(f"insert into estado(id, uf) values({cod_uf}, '{uf}');")
+        #print(f"insert into municipio values({cod_mun}, '{mun}', {cod_uf});")
+        
+        if not cod_uf in dic_ufs:
+            saida.write(f"insert into estado(id, uf) values({cod_uf}, '{uf}');")
+            saida.write("\n")
+            dic_ufs[cod_uf] = cod_uf
+
+        saida.write(f"insert into municipio values({cod_mun}, '{mun}', {cod_uf});")
+        saida.write("\n")
+
+saida.close()
