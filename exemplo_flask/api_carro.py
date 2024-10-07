3eb22ace552d05f446d3abb73501ea02bf002550
Eduardo Gondo <egondo@gmail.com>
2024-10-07 11:00:56 -0300

    Flask continuacao

--- exemplo_flask/api_carro.py
+++ exemplo_flask/api_carro.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/carros", methods=["GET"])
 def get_all_carros():
-    return  (db.carros, 200)
+    return  (jsonify(db.carros), 200)
 
 @app.route("/carros/<int:id>", methods=["GET"])
 def get_carro(id):    
@@ -26,6 +26,29 @@ def insere_carro():
     db.carros.append(dado)
     return jsonify(dado), 201
 
+@app.route("/carros", methods=["PUT"])
+def altera_carro():
+    dado = request.json
+    for car in db.carros:
+        if car['id'] == dado['id']:
+            car['modelo'] = dado['modelo']
+            car['montadora'] = dado['montadora']
+            car['ano'] = dado['ano']
+            car['placa'] = dado['placa']
+            return (car, 200)
+        
+    return {'title': 'Carro not found', 'status': 404, 
+            "type": "https://fiap.com.br/car_not_found", 
+            "detail": f"Carro nao encontrado com o id especificado {dado['id']}", "instance": f"/carros/{dado['id']}"}, 404
 
+@app.route("/carros/update", methods=["PUT"])
+def altera_carro2():
+    dado = request.json
+    for ind, car in enumerate(db.carros):
+        print(id, car)
+        if car['id'] == dado['id']:
+            db.carros[ind] = dado
+            return dado, 200
+    return {"title": "Carro não encontrado", "status": 404}, 404
 
 app.run(debug=True)
