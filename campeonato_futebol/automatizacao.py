d2272676daec210fe0a5c6975493aabb9561b0df
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-14 09:47:04 -0300

    pyautogui

---
+++ campeonato_futebol/automatizacao.py
@@ -0,0 +1,28 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+
+pyautogui.click(47, 128)
+pyautogui.typewrite("Corinthians")
+
+
+pyautogui.click(197, 128)
+pyautogui.typewrite("2")
+
+pyautogui.click(274, 128)
+pyautogui.typewrite("0")
+
+pyautogui.click(380, 128)
+pyautogui.typewrite("Palmeiras")
+
+pyautogui.click(58, 156)
+
+
+
+
+
+
+#pyautogui.move(50, 50, duration=1)
+
